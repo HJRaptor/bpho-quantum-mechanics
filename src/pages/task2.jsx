37ffb5cffcf4b2b0e04c337b296198e03a07fd52
Harshit <harshit.jagarlamudi@outlook.com>
2026-06-06 12:00:02 +0100

    remove task 2

--- src/pages/task2.jsx
+++ src/pages/task2.jsx
@@ -13,103 +13,103 @@ export default function Task2() {
 
     //constants
 
-    const N = 1000
-    const T = 100
+    // const N = 1000
+    // const T = 100
 
-    const m = 28.96e-3 / 6.022e23
-    const M = 10*m
+    // const m = 28.96e-3 / 6.022e23
+    // const M = 10*m
 
-    const r = 0.16
-    const R = 10*r
+    // const r = 0.16
+    // const R = 10*r
 
-    const a = 7*R
+    // const a = 7*R
 
-    const C = 1
+    // const C = 1
 
-    const k_B = 1.38e-23
+    // const k_B = 1.38e-23
 
-    let v= Math.sqrt(3*k_B*(T+273)/m)
-    let V= Math.sqrt(3*k_B*(T+273)/M)
+    // let v= Math.sqrt(3*k_B*(T+273)/m)
+    // let V= Math.sqrt(3*k_B*(T+273)/M)
 
-    const Kn = 15
+    // const Kn = 15
 
-    let tmax = 200
+    // let tmax = 200
 
-    v = v/1000
-    V = V/1000
+    // v = v/1000
+    // V = V/1000
 
-    let dt = 0.01*Kn*r/v
+    // let dt = 0.01*Kn*r/v
 
-    let stopstop = 0;
+    // let stopstop = 0;
 
-    //ball displacement
-    function ball_displacement(x1,y1,x2,y2){
-        d = Math.sqrt((x2-x1 )**2  + (y2-y1)**2)
-
-
-
-
-    }
-
-
-    while (stopstop == 0){
-
-        let X = 0.5*a
-        let Y = 0.5*a
-
-        let theta = 2 * pi * Math.random()
-        let Vx = V*Math.cos(theta)
-        let Vy = V*Math.sin(theta)
-
-        let x = []
-        let y = []
-
-        for(let n=0; n<=N; n++){
-            d=0
-            while(d < (R+r)){
-                x[n] = r + Math.random*(a-2*r)
-                y[n] = r + Math.random*(a-2*r)
-
-            }
-
-
-        }
-
-    }
+    // //ball displacement
+    // function ball_displacement(x1,y1,x2,y2){
+    //     d = Math.sqrt((x2-x1 )**2  + (y2-y1)**2)
 
 
 
-    let layout = {
 
-        autosize: true,
-        title: { text: `Brownian Motion Simulation 200 ps` },
-        showlegend: true,
+    // }
 
-        shapes: [{
+
+    // while (stopstop == 0){
+
+    //     let X = 0.5*a
+    //     let Y = 0.5*a
+
+    //     let theta = 2 * Math.PI * Math.random()
+    //     let Vx = V*Math.cos(theta)
+    //     let Vy = V*Math.sin(theta)
+
+    //     let x = []
+    //     let y = []
+
+    //     for(let n=0; n<=N; n++){
+    //         let d=0
+    //         while(d < (R+r)){
+    //             x[n] = r + Math.random*(a-2*r)
+    //             y[n] = r + Math.random*(a-2*r)
+
+    //         }
+
+
+    //     }
+
+    // }
+
+
+
+    // let layout = {
+
+    //     autosize: true,
+    //     title: { text: `Brownian Motion Simulation 200 ps` },
+    //     showlegend: true,
+
+    //     shapes: [{
             
-            type: 'rect',
-            x0: 0,
-            y0: 0,
-            x1: 7,
-            y1: 7,
-            line:{
-                color: 'black',
-                width: 5
+    //         type: 'rect',
+    //         x0: 0,
+    //         y0: 0,
+    //         x1: 7,
+    //         y1: 7,
+    //         line:{
+    //             color: 'black',
+    //             width: 5
 
-            }
+    //         }
             
             
-        }]
-    }
+    //     }]
+    // }
     
-    let config = {
-            responsive: true,
-            displayModeBar: false
-        }
+    // let config = {
+    //         responsive: true,
+    //         displayModeBar: false
+    //     }
 
-    useEffect(() => {
-        Plotly.newPlot('myplot',[] ,layout, config);
-    }, []);    
+    // useEffect(() => {
+    //     Plotly.newPlot('myplot',[] ,layout, config);
+    // }, []);    
 
 
 
