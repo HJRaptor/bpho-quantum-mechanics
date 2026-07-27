44ad8ee2eabd79a0f785abad63faf1c6728eece1
AbhinavN1409 <nayakabhinav08@gmail.com>
2026-07-27 17:56:38 +0100

    Task 3

--- src/pages/task3.jsx
+++ src/pages/task3.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input"
 import Navbar from '@/components/navbar';
+import image1 from '@/assets/PlanckSpectrum.svg';
+import image2 from '@/assets/EinsteinsModel.svg';
 
 export default function Task3() {
 
@@ -13,6 +15,10 @@ export default function Task3() {
     return (
         <>    
 
+    <body>
+        <img src={image1} alt="MATLAB Line Plot" width="600"></img>
+        <img src={image2} alt="MATLAB Line Plot" width="600"></img>
+    </body>
         </>
     );
 
