75a921320c917e7d7a696e93160ff9b746ad1c9c
Harshit <harshit.jagarlamudi@outlook.com>
2026-08-02 10:33:09 +0100

    homepage

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -8,9 +8,11 @@ export default function Homepage() {
 
             {/* Main Content */}
             <main className="container mx-auto px-4 py-8 text-center">
-                <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+                <h1 className="text-8xl font-extrabold tracking-tight mb-6">
                     BPhO 2026 Computational Challenge
                 </h1>
+                <h1 className='text-8xl font-extrabold tracking-tight mb-6'>Quantum Mechanics</h1>
+                <h1>Abhinav </h1>
             </main>
         </>
     );
