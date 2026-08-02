d5d21f1a43aff2d2f925817c3dceb3102ff7b186
AbhinavN1409 <nayakabhinav08@gmail.com>
2026-08-02 10:32:09 +0100

    Task 6 and 10

--- src/pages/task3.jsx
+++ src/pages/task3.jsx
@@ -1,9 +1,4 @@
 import React, { useEffect } from 'react';
-import Plotly from 'plotly.js-dist';
-import { Link } from 'react-router-dom';
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input"
-import Navbar from '@/components/navbar';
 import image1 from '@/assets/PlanckSpectrum.svg';
 import image2 from '@/assets/EinsteinsModel.svg';
 
