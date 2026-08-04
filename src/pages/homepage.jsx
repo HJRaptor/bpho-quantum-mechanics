7a598b2c2311033345a8c41e2c7c268c0c0d2149
Harshit <harshit.jagarlamudi@outlook.com>
2026-08-04 23:25:57 +0100

    initial simulation setup of photoelectric effect

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,6 +2,136 @@ import { Link } from 'react-router-dom';
 import { Button } from "@/components/ui/button"; // shadcn button component
 
 export default function Homepage() {
+
+    //implementing photoelectric effect demo logic
+
+    const metals_ev = [4.3, 4.3, 5.1, 4.7, 4.4, 4.3, 4.5, 4.6, 2.4]
+    const metals_names = ['Silver', 'Aluminium', 'Gold', 'Copper', 'Tin', 'Lead', 'Tungsten', 'Nickel', 'Sodium']
+
+    const H = 6.63 * 10**(-34)
+    const C = 3 * 10**8
+    const e = 1.6 * 10**(-19)
+    const metal_plate_area = 1 * 10**(-4)
+
+    const default_metal = metals_names[0]
+    const default_wf = metals_ev[0]
+
+
+    //maps wavelengths of light to a colour code
+    function wavelengthtorgb(lamda){
+        let r = 0
+        let g = 0
+        let b = 0
+
+        if (lamda >= 380 && lamda < 440) {
+            r = -(lamda - 440) / (440 - 380)
+            g = 0
+            b = 1
+        } else if (lamda >= 440 && lamda < 490) {
+            r = 0
+            g = (lamda - 440) / (490 - 440)
+            b = 1
+        } else if (lamda >= 490 && lamda < 510) {
+            r = 0
+            g = 1
+            b = -(lamda - 510) / (510 - 490)
+        } else if (lamda >= 510 && lamda < 580) {
+            r = (lamda - 510) / (580 - 510)
+            g = 1
+            b = 0
+        } else if (lamda >= 580 && lamda < 645) {
+            r = 1
+            g = -(lamda - 645) / (645 - 580)
+            b = 0
+        } else if (lamda >= 645 && lamda <= 780) {
+            r = 1
+            g = 0
+            b = 0
+        } else {
+            
+            return '#808080'
+
+            
+        }
+
+        //correct for intensity of colours
+        let factor = 1;
+        if (lamda >= 380 && lamda < 420) {
+            factor = 0.3 + 0.7 * (lamda - 380) / (420 - 380)
+        } else if (lamda >= 420 && lamda < 701) {
+            factor = 1.0
+        } else if (lamda >= 701 && lamda <= 780) {
+            factor = 0.3 + 0.7 * (780 - lamda) / (780 - 700)
+        }
+
+
+        function rgbtohex(r, g, b, factor) {
+
+
+            function tohex(colorValue) {
+
+                const normalisedvalue = Math.round(colorValue * factor * 255)
+
+                const hexstring = normalisedvalue.toString(16)
+
+                return hexstring.padStart(2, '0')
+            }
+
+            // 4. Combine the channels with a leading hash symbol
+            const red = tohex(r)
+            const green = tohex(g)
+            const blue = tohex(b)
+
+            return `#${red}${green}${blue}`
+        }
+
+
+
+    }
+
+
+    //variables 
+    const [intensity, setIntensity] = useState(100)//W/m^2   
+    const [wavelength, setWavelength] = useState(550)//nm
+    const [cMetal, setSelectedMetal] = useState(default_metal)
+    const [cCurrent, setDisplayedCurrent] = useState(0)//A
+
+    
+    function calcwf(metal){
+        let index = metals_names.indexOf(metal)
+        let wf = metals_ev[index] * e
+        return wf // joules
+
+    }
+
+
+    const stateRef = useRef({
+        intensity: 100,
+        wavelength: 550,
+        wf: calcwf(default_metal),
+        metal: default_metal,
+    });
+
+    const canvasRef = useRef(null)
+    const photonsRef = useRef([])
+    const electronsRef = useRef([])
+
+
+    useEffect(() => {
+        stateRef.current = {
+            intensity,
+            wavelength,
+            workFunction: getWorkFunction(selectedMetal),
+            metalName: selectedMetal,
+        }
+    }, [intensity, wavelength, cMetal])
+
+
+
+
+
+
+
     return (
         <>
 
