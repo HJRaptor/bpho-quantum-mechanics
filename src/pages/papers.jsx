81436e5159e1a44931151590851663431087f6ad
Harshit <harshit.jagarlamudi@outlook.com>
2026-08-09 23:51:28 +0100

    Upload research papers Add links to Papers page

--- src/pages/papers.jsx
+++ src/pages/papers.jsx
@@ -6,6 +6,15 @@ export default function Papers() {
 
     return (
         <>
+        <div className='flex flex-col justify-center m-4 h'>
+
+
+            <a href="/Research papers/Nuclear_Problem_Sheet.pdf"  target="_blank" rel="noopener noreferrer" ><h1 className='text-4xl hover:text-green-700'>Problem Sheet 1 - Nuclear</h1></a>
+            <a href="/Research papers/Hydrogenic Orbitals Paper.pdf"  target="_blank" rel="noopener noreferrer" ><h1 className='text-4xl hover:text-green-700'>Hydrogenic Orbitals</h1></a>
+            <a href="/Research papers/BPhO_Computational_Challenge_Question_6___Electron_Diffraction.pdf"  target="_blank" rel="noopener noreferrer" ><h1 className='text-4xl hover:text-green-700'>Task 6 Electron Diffraction Writeup</h1></a>
+
+
+        </div>
         </> 
     );
 
